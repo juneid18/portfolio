b0dea47ede179c7cf6e02ce29c48bcb6d9291c3b
Juneid Shaikh <90983910+juneid18@users.noreply.github.com>
2026-03-15 18:24:28 +0530

    feat: Add Sanity client configuration.

--- app/client.js
+++ app/client.js
@@ -4,7 +4,7 @@ export const client = createClient({
   projectId: "zmklohac",
   dataset: "production",
   apiVersion: "2024-01-01",
-  token: process.env.SANITY_WRITE_TOKEN,
+  token: "skFH319YVbmQUYL0c8kZre8fs2pA24JyoAhUaL5V1wLlmFXWsCEpaenB7xkThMYh80lspIdEiq9kDEhFLmYR9ah2lHLr5nuNrtMudBqZnPrF8yJEm9K7DayDUe8E3mq49QstJTtupfFzSYfiaanYuDOv0ye1SqmDGRlioxB5leEWLfYBMVNA",
   useCdn: false,
 });
 
